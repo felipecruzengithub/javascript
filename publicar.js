b59a2d60ed43e1d38c3780e9957a70c18fdb2764
felipecruzengithub <37128980+felipecruzengithub@users.noreply.github.com>
2024-01-21 21:50:12 -0500

    Create publicar.js

---
+++ publicar.js
@@ -0,0 +1,15 @@
+alert('"¡Bienvenida y bievenido a nuestro sitio web!"');
+let nombre = "luna";
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+alert('"¡Error! Completa todos los campos"');
+let mensajeDeError = "¡Error! Completa todos los campos"; 
+alert('mensajeDeError');
+let nombreUsuario = prompt('ingrese su nombre de usuario:');
+let edadUsuario = prompt('ingrese su edad:');
+
+if (edadUsuario >=18)
+{
+    alert('"¡Puedes obtener tu licencia de conducir!"');
+}
