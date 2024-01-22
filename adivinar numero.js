48ad4b69ce9f265346d0f86bec6c8067e3e2b334
felipecruzengithub <37128980+felipecruzengithub@users.noreply.github.com>
2024-01-21 21:48:40 -0500

    Create adivinar numero.js

---
+++ adivinar numero.js
@@ -0,0 +1,14 @@
+prompt('Elige un número entre 1 y 10');
+alert('"¡Bienvenida y bievenido a nuestro sitio web!"');
+
+let mensajeDeBienvenida = 'Bienvenido a la aplicación';
+alert(mensajeDeBienvenida);
+
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un n�mero por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert(`Acertaste el n�mero: ${numeroUsuario}`);
+}
